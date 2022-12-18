1181c8bf44e2e2bd7bc90e5328ea4a9eda0d8259
coldestengine <118161380+coldestengine@users.noreply.github.com>
2022-12-18 21:13:01 +0700

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -115,10 +115,8 @@ USE_TZ = True
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
