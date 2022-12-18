3cda66f2cdee905d97dc0afdaa3e0dcacebf112f
coldestengine <118161380+coldestengine@users.noreply.github.com>
2022-12-18 18:32:11 +0700

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,5 +135,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ('kristianto222@gmail.com') #os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = ('dhdagtxtjydgdfyl') #os.environ.get('EMAIL_PASS')
