03d0ac48a4bffad9724eaa6c92b62d627d66df9a
coldestengine <118161380+coldestengine@users.noreply.github.com>
2022-12-18 18:19:06 +0700

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/home/aegonwons/django_project/django_project/blog'
+# STATIC_ROOT = '/home/aegonwons/django_project/django_project/blog'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = "static/"
 
